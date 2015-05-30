ec3b4d52f5519fc88946f5c1f9c3c518af93165b
Tim Krajcar <allegro@conmolto.org>
2015-05-30 13:49:56 -0700

    receive -> record

--- experiments/record.rb
+++ experiments/record.rb
@@ -2,7 +2,6 @@ require "midi"
 require "json"
 
 # prompt the user to select an input and output
-
 @input = UniMIDI::Input.gets
 @output = UniMIDI::Output.use(:first)
 starting_time = Time.now
@@ -16,7 +15,6 @@ MIDI.using(@input, @output) do
   receive(:note_on, :note_off) do |note|
     if note.velocity == 0
       mynote = @current_notes.delete note.note
-#      require 'pry';binding.pry
       mynote[:duration] = Time.now - mynote[:starting_time]
       mynote.delete :starting_time
       @all_notes << mynote
